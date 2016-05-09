8dc779f80d0d79a6716acc3304c4d317835e5182
denipanova <denitsa.panova@barcelonagse.eu>
2016-05-09 11:22:59 +0200

    master table small changes

--- master_table_creation.R
+++ master_table_creation.R
@@ -1,10 +1,10 @@
 library(data.table)
 
-setwd("~/Desktop/BGSE/Term3/MasterProject/GSE")
+#setwd("~/Desktop/BGSE/Term3/MasterProject/GSE")
 
 ## Read in Sales table
-# sales<-readRDS("/home/didi/BGSE/semester3/kernel/data/sales.RData")
-sales <- readRDS("sales.RData")
+sales<-readRDS("/home/didi/BGSE/semester3/kernel/data/sales.RData")
+#sales <- readRDS("sales.RData")
 
 # Match the date to the assortment files
 sales <- sales[date > "2014-12-31"]
@@ -20,9 +20,12 @@ sum(a$daily_records!=b$uni_daily_records)
 
 
 ## Read in the assortment table
-# assortment_date = readRDS("/home/didi/BGSE/semester3/kernel/data/product_store_timeline_total_days_sales.RData")
-assortment_date <- readRDS("product_store_timeline_total_days.RData")
+# USE THE NEW DATA
+assortment_date = readRDS("/home/didi/BGSE/semester3/kernel/data/product_store_timeline_total_days.RData")
+#assortment_date <- readRDS("product_store_timeline_total_days.RData")
 names(assortment_date)[3] <- "days_in_assort"
+#Deni: I don't know why but i have 2 columns total_days so i will drop one
+#assortment_date[,total_days:=NULL]
 
 # aggregate by time and put into the master table
 master_table <- sales[,.(total_quantity = sum(quantity_sold_kg)), by=.(productID,storeID)]
@@ -44,11 +47,11 @@ s_product - ass_product #-54
 ################################################################################
 
 
-## From sales, remove any entries for which we don't have assortment data
-master_table <- master_table[storeID %in% assortment_date[,storeID]]
-length(unique(master_table[,storeID])) #2666 (< 2682)
-# We lose more than 148 entries which means there were also stores in the assortment data which we didn't have in sales.
-# Note - we still have more products in assortment 
+# ## From sales, remove any entries for which we don't have assortment data
+# master_table <- master_table[storeID %in% assortment_date[,storeID]]
+# length(unique(master_table[,storeID])) #2666 (< 2682)
+# # We lose more than 148 entries which means there were also stores in the assortment data which we didn't have in sales.
+# # Note - we still have more products in assortment 
 
 
 ## Include 'no. of days in assortment' and 'ave sales per day' columns.  
@@ -61,7 +64,9 @@ master_table <- master_table[days_in_assort > 5]
 
 
 ## Add store variables to master table (note. promo_group not included since it's changing)
-stores <- readRDS("stores.RData")
+#stores <- readRDS("stores.RData")
+stores = readRDS("/home/didi/BGSE/semester3/kernel/data/stores.RData")
+#removing date and promo_group
 stores[,c("date","promo_group") := list(NULL, NULL)]
 stores <- unique(stores) #3326
 #check if there are any stores in master table that are not in stores table
@@ -69,7 +74,8 @@ length(setdiff(master_table$storeID, stores$storeID)) #No
 master_table <- merge(master_table, stores, by="storeID")
 
 ## Add province and autonomous community data. NB we only have around 2 thirds of the stores here, so we have ~106865 NA's
-regions <- readRDS("province.RData")
+#regions <- readRDS("province.RData")
+regions <- readRDS("/home/didi/BGSE/semester3/kernel/data/province.RData")
 regions <- unique(regions) #1781
 # find storeID's common to both master table and regions table
 stores_used  <- intersect(regions$ipdv, master_table$storeID)
@@ -79,7 +85,8 @@ length(setdiff(regions$ipdv, master_table$storeID)) #correct
 master_table <- merge(master_table, regions, by.x="storeID", by.y="ipdv", all=TRUE)
 
 ## Add product variables to master table
-products <- readRDS("products.RData")
+#products <- readRDS("products.RData")
+products <- readRDS("/home/didi/BGSE/semester3/kernel/data/products.RData")
 products[,c("iniDate", "endDate") := list(NULL, NULL)]
 products <- unique(products) # doesn't change anything. Already unique.
 #check there are no products in master table that are not in products table
@@ -111,7 +118,8 @@ dim(master_table)[1] - dim(c)[1] #91,739 entries lost
 is.na(master_table$size) <- which(master_table$size == "")
 
 # save master table
-saveRDS(master_table, file="master_table.RData")
+#saveRDS(master_table, file="master_table.RData")
+saveRDS(master_table, file="/home/didi/BGSE/semester3/kernel/data/master_table.RData")
 
 
 
