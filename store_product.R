e0df1fa052953b12d90505aab6ce21c56aaf891a
denipanova <denitsa.panova@barcelonagse.eu>
2016-05-16 11:44:09 +0200

    mode is added to the file of store_product_shelf_space

--- store_product.R
+++ store_product.R
@@ -12,7 +12,7 @@ file.names <- dir(path, pattern =".RData")
 file.names = file.names[-length(file.names)]
 
 store_product = readRDS("/home/didi/BGSE/semester3/kernel/data/product_store_timeline_total_days.RData") 
-store_product[,total_days:=NULL]
+#store_product[,total_days:=NULL]
 
 #create temporary table through which to loop
 store_product_temp = store_product
@@ -43,41 +43,54 @@ for(i in 1:length(file.names)){
 
 saveRDS(store_product,"../store_product.RData") 
 ### define mode function
-Mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
 
 
 ### Divide it into 2 parts for memory reasons
 n =nrow(store_product)
 # first part
-rm(store_product)
 store_product_half = store_product[1:(n/2),]
+rm(store_product)
 #total shelf space
-store_product_half[,total_shelf_space := rowSums(store_product_half[,5:441, with = FALSE])]
-store_product_half[,average_shelf_space := (total_shelf_space/total_days)]
-store_product_half[,mode_shelf_space := names(sort(-table((store_product_half[,5:441, with = FALSE]))))[1]]
+store_product_half[,total_shelf_space := rowSums(store_product_half[,5:442, with = FALSE])]
+saveRDS(store_product_half,"/home/didi/BGSE/semester3/kernel/data/store_product_hald.RData")
+
 #second part
 store_product = readRDS("../store_product.RData")
-rm(store_product)
 store_product_half2 = store_product[((n/2)+1):n,]
+rm(store_product)
 #total shelf_space
-store_product_half2[,total_shelf_space := rowSums(store_product_half2[,5:441, with = FALSE])]
-store_product_half2[,average_shelf_space := (total_shelf_space/total_days)]
-
+store_product_half2[,total_shelf_space := rowSums(store_product_half2[,5:442, with = FALSE])]
 saveRDS(store_product_half2,"/home/didi/BGSE/semester3/kernel/data/store_product_hald2.RData")
+
+
+
+#Calculating the mode
+#for the first half
+store_product_half[,mode_shelf_space := NA]
+
+for(i in 200:nrow(store_product_half)){
+  print(i)
+  row = as.numeric(as.vector(store_product_half[i,5:442, with=F]))
+  mode = unique(row)[which.max(tabulate(match(row,unique(row))))] 
+  store_product_half[i,mode_shelf_space:=mode]
+} 
+
+# for the second half
+store_product_half2[,mode_shelf_space := NA]
+
+for(i in 1:nrow(store_product_half2)){
+  print(i)
+  row = as.numeric(as.vector(store_product_half2[i,5:442, with=F]))
+  mode = unique(row)[which.max(tabulate(match(row,unique(row))))] 
+  store_product_half2[i,mode_shelf_space:=mode]
+} 
+
+
 #read the first half
 store_product_half = readRDS("/home/didi/BGSE/semester3/kernel/data/store_product_half.RData")
 #combine both
 store_product = rbind(store_product_half,store_product_half2)
 saveRDS(store_product,"/home/didi/BGSE/semester3/kernel/data/store_product.RData")
-
-
-
-
-
-
 
 
 
