c7e7963f8a5eaf9ed58233c62f8ab32305656964
aspeijers <anneke.speijers@barcelonagse.eu>
2016-06-12 13:15:26 +0200

    svm modelling code

--- sales_modelling2.R
+++ sales_modelling2.R
@@ -1,19 +1,27 @@
-# this file carries out sales modelling for (storeID, productID) tuples in the store_test data
+# this file carries out sales modelling for (storeID, productID) tuples.
+# data has been split randomly and test data has (some) mkt share variables calculated from clusters
+# the model is trained and parameters tuned using the training data and CV.
+# we then use the model to predict sales for productID's that do not currently exist in stores.
+
 
 # load packages
 library(data.table)
-library(h2o)
+library(caret)
+library(randomForest)
+library(doMC)
 
+# load training data and validation test data 
+# the test dta
 train <- readRDS("store_train.RData")
-test <- readRDS("store_test.RData")
+# test <- readRDS("store_test.RData")
 
 # remove columns not used in model
 train[,c("total_quantity", "days_in_assort", "height", "expirationDays",
                 "width", "depth", "availability", "total_shelf_space", 
                 "mode_shelf_space"):= NULL]
-test[,c("total_quantity", "days_in_assort", "height", "expirationDays",
-         "width", "depth", "availability", "total_shelf_space", 
-         "mode_shelf_space"):= NULL]
+# test[,c("total_quantity", "days_in_assort", "height", "expirationDays",
+#          "width", "depth", "availability", "total_shelf_space", 
+#          "mode_shelf_space"):= NULL]
 
 # columns to treat as factors
 factor_vars <- c("productID", "storeID", "grup", "fam", "subFam", "sub_chain",
@@ -22,87 +30,104 @@ factor_vars <- c("productID", "storeID", "grup", "fam", "subFam", "sub_chain",
 
 # update necessary columns to factors
 train <- train[,(factor_vars):=lapply(.SD, as.factor),.SDcols=factor_vars, with=FALSE]
-test <- test[,(factor_vars):=lapply(.SD, as.factor),.SDcols=factor_vars, with=FALSE]
+# test <- test[,(factor_vars):=lapply(.SD, as.factor),.SDcols=factor_vars, with=FALSE]
 rm(factor_vars)
 
 # put response variable in first column
-neworder <- c(names(train)[9], names(train)[-9])
+neworder <- c(names(train)[-9], names(train)[9])
 setcolorder(train, neworder)
 rm(neworder)
 
 # set up data to use in predict function 
-predict_dt <- as.data.table(as.data.frame(test))[,avg_sales_per_day:=NULL]
+# predict_dt <- as.data.table(as.data.frame(test))[,avg_sales_per_day:=NULL]
 
 
 
 ################################################################################
-# Create Random Forest model 
+        # model using all variables, then do variable selection #
 ################################################################################
+# Base line RF model - default parameters (2 folds)
+control <- trainControl(method="cv", number=2, verboseIter = TRUE)
+seed <- 7
+metric <- "RMSE"
+set.seed(seed)
+mtry <- sqrt(ncol(train)-1)
+tunegrid <- expand.grid(.mtry=mtry)
 
-# master_train must have the response variable in the first column
-rf_model <- function(training_df, ntrees=50, nfolds=5, mtries=-1) {
-    
-    # install and load pacakges
-    if (!require("h2o")) install.packages("h2o"); library(h2o)
-    
-    # initialise h2o
-    h2o.init(nthreads = -1)
-    
-    # Map to h2o data frame
-    train.h2o   <- as.h2o(training_df, destination_frame = "training")
-    
-    # run RF model
-    model <- h2o.randomForest( x=2:ncol(training_df),
-                               y=1, 
-                               training_frame=train.h2o, 
-                               nfolds=nfolds,
-                               mtries=mtries,
-                               ntrees=ntrees)
-    return(model)
-}
+registerDoMC(cores = 2)
+rf_default <- train(avg_sales_per_day~., 
+                    data=train, 
+                    method="rf", 
+                    metric=metric, 
+                    tuneGrid=tunegrid, 
+                    trControl=control, 
+                    importance=TRUE) #approx 3hrs
+print(rf_default)
+#varImp(rf_default) #need to set importance=TRUE in train() to be able to get this
+#varImp(rf_default$finalModel)
+rf_default$finalModel
+rf_default$results
+rf_default$times
 
-
-
-################################################################################
-# Predict for existing (storeID, productID) tuples
-################################################################################
-# 'predict_dt' must be class data.table
-predict_model <- function(predict_dt, model) {
-    
-    # install and load pacakges
-    if (!require("h2o")) install.packages("h2o"); library(h2o)
-    
-    # initialise h2o
-    h2o.init(nthreads = -1)
-    
-    # map to h2o object
-    predict.h2o    <- as.h2o(predict_dt, destination_frame = "predict")
-    
-    # predictions
-    preds <- h2o.predict(model, newdata = predict.h2o)
-    preds <- as.data.frame(preds)
-    
-    # add storeID and productID to preds
-    preds <- cbind(predict_dt[,.(storeID, productID)], preds)
-    
-    return(preds)
-    
-}
-
-
-
-# run the model
-rf_model_1 <- rf_model(train, nfolds=2)
-
-
-# idea is to predict sales for the test data using the mkt share variables we already 
-# have (ie predict for products that already exist in a store) and then
-# artificially create the mkt share variable (since in real likfe we don't have them)
-# We can then compare how well are model predicts for these scenarios.
-
+# save workspace
+save.image("~/Desktop/BGSE/Term3/MasterProject/GSE/rf_default.RData")
+rm(rf_default)
 
 
 ################################################################################
-# create artificially the mkt share variable for the test data
-################################################################################
+# Base line RF model - default parameters (2 folds, variable importance returned)
+control <- trainControl(method="cv", number=2, verboseIter = TRUE)
+seed <- 7
+metric <- "RMSE"
+set.seed(seed)
+mtry <- sqrt(ncol(train)-1)
+tunegrid <- expand.grid(.mtry=mtry)
 
+registerDoMC(cores = 2)
+rf_default <- train(avg_sales_per_day~., 
+                    data=train, 
+                    method="rf", 
+                    metric=metric, 
+                    tuneGrid=tunegrid, 
+                    trControl=control, 
+                    importance=TRUE) # approx 33 hrs
+print(rf_default)
+varImp(rf_default)
+varImp(rf_default$finalModel)
+rf_default$finalModel
+rf_default$results
+rf_default$times
+
+# save workspace
+save.image("~/Desktop/BGSE/Term3/MasterProject/GSE/rf_default_varImps.RData")
+rm(rf_default)
+
+
+################################################################################
+# Base line RF model - random search for parameters (2 folds -  no parallelization)
+control <- trainControl(method="cv", number=2, verboseIter = TRUE, search="random")
+seed <- 7
+metric <- "RMSE"
+set.seed(seed)
+mtry <- sqrt(ncol(train)-1)
+
+detach("package:doMC", unload=TRUE)
+#registerDoMC(cores = 2)
+rf_random <- train(avg_sales_per_day~., 
+                     data=train, 
+                     method="rf", 
+                     metric=metric, 
+                     tuneLength=15, 
+                     trControl=control, 
+                     importance=FALSE)
+print(rf_random)
+plot(rf_random)
+#varImp(rf_random) 
+#varImp(rf_default$finalModel)
+rf_random$finalModel
+rf_random$results
+rf_random$times
+
+# save workspace
+save.image("~/Desktop/BGSE/Term3/MasterProject/GSE/rf_random.RData")
+rm(rf_random)
