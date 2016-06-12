9b294f53da6cdd1c1179b68a64ba55c5163854a8
denipanova <denitsa.panova@barcelonagse.eu>
2016-06-12 13:13:11 +0200

    cross validation function for the cluster ecaluation completed, cluster 3 is the best one

--- CV_cluster_evaluation.R
+++ CV_cluster_evaluation.R
@@ -1,41 +1,13 @@
 ##### Evaluate clusters
 setwd("~/BGSE/semester3/kernel/data")
+
 library(data.table)
 library(plyr)
-train = readRDS("store_train.RData")
-test = readRDS("store_test.RData")
-clusters = readRDS("kmeans_clusters.RData")
-cluster = clusters[,1:2]
-rm(clusters)
-names(cluster)[2] ="cluster"
-
-#identify which grup has only 1 or 2 or 3 products 
-products = readRDS("products.RData")
-products = products[availability == "alltimer"]
-group_statistics  = products[,.(num_products = length(productID)), by = grup]
-avg_groups = c()
-for(i in group_statistics$grup){
-  if (group_statistics[grup==i,]$num_products == 1 |
-      group_statistics[grup==i,]$num_products == 2 |
-      group_statistics[grup==i,]$num_products == 3){
-    avg_groups = c(avg_groups,i)
-  }
-}
 
 
-#identify which flavor has only 1 or 2 or 3 products 
-flavor_statistics  = products[,.(num_products = length(productID)), by = flavor]
-avg_flavor= c()
-for(i in flavor_statistics$flavor){
-  if (flavor_statistics[flavor==i,]$num_products == 1 |
-      flavor_statistics[flavor==i,]$num_products == 2 |
-      flavor_statistics[flavor==i,]$num_products == 3){
-    avg_flavor = c(avg_flavor,i)
-  }
-}
 
-rm(products,flavor_statistics,group_statistics)
-
+###################################################################################################################################
+################### MSE FUNCTOIONS#################################################################################################
 
 
 mkt_MSE = function(train,test, cluster,avg_flavor,avg_groups){
@@ -47,7 +19,7 @@ mkt_MSE = function(train,test, cluster,avg_flavor,avg_groups){
   
   ### w.r.t. product 
   print("product")
-  avg_variables = c("mkt_product_store","mkt_Fam_store")
+  avg_variables = c("mkt_product_store","mkt_product_store_sales")
   train_product = train[,lapply(.SD,mean), by =.(cluster,productID),.SDcols = avg_variables ]
   names(train_product)[3:ncol(train_product)] = paste0(avg_variables,"_predict")
   #merge to test 
@@ -76,106 +48,192 @@ mkt_MSE = function(train,test, cluster,avg_flavor,avg_groups){
   
   ### w.r.t. grup 
   print("grup")
-  # subset training data for grup that needs to be averaged over all stores in a particular cluster
-  temp = train[grup%in%avg_groups]
-  train_grup_average = temp[,.(mkt_Grup_store_predict=mean(mkt_Grup_store),
-                               mkt_Grup_store_sales_predict=mean(mkt_Grup_store_sales))
-                            , by = .(cluster,grup)]
-  #artificially create stores
-  store_cluster_grup = test[,.(storeID,grup,cluster)]
-  #there are duplicates here, take the unique rows
-  store_cluster_grup = unique(store_cluster_grup)
-  store_cluster_grup = store_cluster_grup[grup%in%avg_groups]
-  #merge the averaging estimation for every store
-  train_grup_average = merge(store_cluster_grup,train_grup_average, by = c("cluster","grup"),all.x = TRUE)
-  #remove cluster
-  train_grup_average = train_grup_average[,cluster:=NULL]
-  rm(store_cluster_grup)
-  rm(temp)
   
   #subset for the ones which are not in avering part
-  temp2 = train[!(grup%in%avg_groups)]
-  train_grup_not_average = temp2[,.(mkt_Grup_store_predict=unique(mkt_Grup_store),
+  temp = train[!(grup%in%avg_groups)]
+  train_grup_not_average = temp[,.(mkt_Grup_store_predict=unique(mkt_Grup_store),
                                     mkt_Grup_store_sales_predict=mean(mkt_Grup_store_sales)), 
                                  by = .(storeID,grup)]
-  rm(temp2)
-  
-  #merge averaging and not-averaging
-  mkt_Grup_store_predict = rbind(train_grup_not_average,train_grup_average)
-  rm(train_grup_not_average,train_grup_average)
-  
-  # If NAs occur take the average across cluster
-  if (is.na(mkt_Grup_store_predict$mkt_Grup_store_predict )| 
-      is.na(mkt_Grup_store_predict$mkt_Grup_store_sales_predict)) {
-    train_grup_average = train[,.(storeID,mkt_Grup_store_predict=mean(mkt_Grup_store),
-                                 mkt_Grup_store_sales_predict=mean(mkt_Grup_store_sales))
-                              , by = .(cluster,grup)]
-    
-  }
-
+  rm(temp)
   
 
   #merge to test 
-  test = merge(test,mkt_Grup_store_predict,by=c("storeID","grup"), all.x = TRUE)
-  rm(mkt_Grup_store_predict)
+  test = merge(test,train_grup_not_average,by=c("storeID","grup"), all.x = TRUE)
+  rm(train_grup_not_average)
+  ############################################################################
+  # Getting rid of the NAs in the test data by averiging w.r.t. clusters
+  # add store_grup variable to train, test
+  train[,cluster_grup:=paste0(cluster,grup)]
+  test[,cluster_grup:=paste0(cluster,grup)]
   
+  # subset training data for grup that needs to be averaged over all stores in a particular cluster
+  cluster_grup_na = unique(test[is.na(mkt_Grup_store_predict),]$cluster_grup)
+  temp = train[cluster_grup %in% cluster_grup_na]
+  train_grup_average = unique(temp[,.(cluster_grup,mkt_Grup_store_predict=mean(mkt_Grup_store),
+                               mkt_Grup_store_sales_predict=mean(mkt_Grup_store_sales))
+                            , by = .(cluster,grup)])
+  #add storeIDs
+  store_cluster_grup = unique(test[is.na(mkt_Grup_store_predict),.(cluster_grup,storeID)])
+  #merge the calculated mkt based on cluster and grup
+  store_cluster_grup = merge(store_cluster_grup,
+                             train_grup_average[,.(cluster_grup,mkt_Grup_store_sales_predict,mkt_Grup_store_predict)], 
+                             by ="cluster_grup",all.x = TRUE)
+  #merge the averaging estimation for every store if mkt share is NA
+  test[,mkt_Grup_store_predict:=ifelse(is.na(mkt_Grup_store_predict),
+                                       store_cluster_grup[cluster_grup==cluster_grup,mkt_Grup_store_predict],
+                                       mkt_Grup_store_predict)]
   
+  test[,mkt_Grup_store_sales_predict:=ifelse(is.na(mkt_Grup_store_sales_predict),
+                                       store_cluster_grup[cluster_grup==cluster_grup,mkt_Grup_store_sales_predict],
+                                       mkt_Grup_store_sales_predict)]
+  
+  #remove auxiliary cluster
+  rm(train_grup_average,store_cluster_grup,temp)
+
+  #Null the auxiliary columns 
+  train[,cluster_grup:=NULL]
+  test[,cluster_grup:=NULL]
   
   
   
   ### w.r.t. flavor 
   print("flavor")
-  # subset training data for var that need to be averaged over all stores in a particular cluster
-  temp = train[flavor%in%avg_flavor]
-  train_flavor_average = temp[,.(mkt_Flavor_store_predict=mean(mkt_Flavor_store),
-                               mkt_Flavor_store_sales_predict=mean(mkt_Flavor_store_sales))
-                            , by = .(cluster,flavor)]
-  #artificially create stores
-  store_cluster_flavor = test[,.(storeID,flavor,cluster)]
-  #there are duplicates here, take the unique rows
-  store_cluster_flavor = unique(store_cluster_flavor)
-  store_cluster_flavor = store_cluster_flavor[flavor%in%avg_flavor]
-  #merge the averaging estimation for every store
-  train_flavor_average = merge(store_cluster_flavor,train_flavor_average, by = c("cluster","flavor"),all.x = TRUE)
-  #remove cluster
-  train_flavor_average = train_flavor_average[,cluster:=NULL]
-  rm(store_cluster_flavor)
-  rm(temp)
-  
+
   #subset for the ones which are not in avering part
-  temp2 = train[!(flavor%in%avg_flavor)]
-  train_flavor_not_average = temp2[,.(mkt_Flavor_store_predict=unique(mkt_Flavor_store),
-                                    mkt_Flavor_store_sales_predict=mean(mkt_Flavor_store_sales)), 
-                                 by = .(storeID,flavor)]
-  rm(temp2)
-  
-  #merge averaging and not-averaging
-  mkt_Flavor_store_predict = rbind(train_flavor_not_average,train_flavor_average)
-  rm(train_flavor_not_average,train_flavor_average)
+  train_flavor_not_average = train[,.(mkt_Flavor_store_predict=unique(mkt_Flavor_store),
+                                   mkt_Flavor_store_sales_predict=mean(mkt_Flavor_store_sales)), 
+                                by = .(storeID,flavor)]
+
   
   #merge to test 
-  test = merge(test,mkt_Flavor_store_predict,by=c("storeID","flavor"), all.x = TRUE)
-  rm(mkt_Flavor_store_predict)
+  test = merge(test,train_flavor_not_average,by=c("storeID","flavor"), all.x = TRUE)
+  rm(train_flavor_not_average)
+  ############################################################################
+  # Getting rid of the NAs in the test data by averiging w.r.t. clusters
+  # add store_flavor variable to train, test
+  train[,cluster_flavor:=paste0(cluster,flavor)]
+  test[,cluster_flavor:=paste0(cluster,flavor)]
   
-  ### w.r.t. type 
-  print("type")
-  train_type = train[,.(mkt_Type_store_predict=unique(mkt_Type_store),
-                                      mkt_Type_store_sales_predict=mean(mkt_Type_store_sales)), 
-                                   by = .(storeID,type)]
-  #merge to test 
-  test = merge(test,train_type,by=c("storeID","type"), all.x = TRUE)
-  rm(train_type)
+  # subset training data for flavor that needs to be averaged over all stores in a particular cluster
+  cluster_flavor_na = unique(test[is.na(mkt_Flavor_store_predict),]$cluster_flavor)
+  temp = train[cluster_flavor %in% cluster_flavor_na]
+  train_flavor_average = unique(temp[,.(cluster_flavor,mkt_Flavor_store_predict=mean(mkt_Flavor_store),
+                                      mkt_Flavor_store_sales_predict=mean(mkt_Flavor_store_sales))
+                                   , by = .(cluster,flavor)])
+  #add storeIDs
+  store_cluster_flavor = unique(test[is.na(mkt_Flavor_store_predict),.(cluster_flavor,storeID)])
+  #merge the calculated mkt based on cluster and flavor
+  store_cluster_flavor = merge(store_cluster_flavor,
+                             train_flavor_average[,.(cluster_flavor,mkt_Flavor_store_sales_predict,mkt_Flavor_store_predict)], 
+                             by ="cluster_flavor",all.x = TRUE)
+  #merge the averaging estimation for every store if mkt share is NA
+  test[,mkt_Flavor_store_predict:=ifelse(is.na(mkt_Flavor_store_predict),
+                                       store_cluster_flavor[cluster_flavor==cluster_flavor,mkt_Flavor_store_predict],
+                                       mkt_Flavor_store_predict)]
+  
+  test[,mkt_Flavor_store_sales_predict:=ifelse(is.na(mkt_Flavor_store_sales_predict),
+                                             store_cluster_flavor[cluster_flavor==cluster_flavor,mkt_Flavor_store_sales_predict],
+                                             mkt_Flavor_store_sales_predict)]
+  
+  #remove auxiliary cluster
+  rm(train_flavor_average,store_cluster_flavor,temp)
+  
+  #Null the auxiliary columns 
+  train[,cluster_flavor:=NULL]
+  test[,cluster_flavor:=NULL]
   
   ### w.r.t. units 
   print("units")
-  train_units = train[,.(mkt_Units_store_predict=unique(mkt_Units_store),
-                        mkt_Units_store_sales_predict=mean(mkt_Units_store_sales)), 
-                     by = .(storeID,units)]
-  #merge to test 
-  test = merge(test,train_units,by=c("storeID","units"), all.x = TRUE)
-  rm(train_units)
-  rm(avg_variables)
+
+  #subset for the ones which are not in avering part
+  train_units_not_average = train[,.(mkt_Units_store_predict=unique(mkt_Units_store),
+                                   mkt_Units_store_sales_predict=mean(mkt_Units_store_sales)), 
+                                by = .(storeID,units)]
   
+  
+  #merge to test 
+  test = merge(test,train_units_not_average,by=c("storeID","units"), all.x = TRUE)
+  rm(train_units_not_average)
+  ############################################################################
+  # Getting rid of the NAs in the test data by averiging w.r.t. clusters
+  # add store_units variable to train, test
+  train[,cluster_units:=paste0(cluster,units)]
+  test[,cluster_units:=paste0(cluster,units)]
+  
+  # subset training data for units that needs to be averaged over all stores in a particular cluster
+  cluster_units_na = unique(test[is.na(mkt_Units_store_predict),]$cluster_units)
+  temp = train[cluster_units %in% cluster_units_na]
+  train_units_average = unique(temp[,.(cluster_units,mkt_Units_store_predict=mean(mkt_Units_store),
+                                      mkt_Units_store_sales_predict=mean(mkt_Units_store_sales))
+                                   , by = .(cluster,units)])
+  #add storeIDs
+  store_cluster_units = unique(test[is.na(mkt_Units_store_predict),.(cluster_units,storeID)])
+  #merge the calculated mkt based on cluster and units
+  store_cluster_units = merge(store_cluster_units,
+                             train_units_average[,.(cluster_units,mkt_Units_store_sales_predict,mkt_Units_store_predict)], 
+                             by ="cluster_units",all.x = TRUE)
+  #merge the averaging estimation for every store if mkt share is NA
+  test[,mkt_Units_store_predict:=ifelse(is.na(mkt_Units_store_predict),
+                                       store_cluster_units[cluster_units==cluster_units,mkt_Units_store_predict],
+                                       mkt_Units_store_predict)]
+  
+  test[,mkt_Units_store_sales_predict:=ifelse(is.na(mkt_Units_store_sales_predict),
+                                             store_cluster_units[cluster_units==cluster_units,mkt_Units_store_sales_predict],
+                                             mkt_Units_store_sales_predict)]
+  
+  #remove auxiliary cluster
+  rm(train_units_average,store_cluster_units,temp)
+  
+  #Null the auxiliary columns 
+  train[,cluster_units:=NULL]
+  test[,cluster_units:=NULL]
+  
+  # w.r.t. Type
+  ### w.r.t. type 
+  print("type")
+  
+  #subset for the ones which are not in avering part
+  train_type_not_average = train[,.(mkt_Type_store_predict=unique(mkt_Type_store),
+                                     mkt_Type_store_sales_predict=mean(mkt_Type_store_sales)), 
+                                  by = .(storeID,type)]
+  
+  
+  #merge to test 
+  test = merge(test,train_type_not_average,by=c("storeID","type"), all.x = TRUE)
+  rm(train_type_not_average)
+  ############################################################################
+  # Getting rid of the NAs in the test data by averiging w.r.t. clusters
+  # add store_type variable to train, test
+  train[,cluster_type:=paste0(cluster,type)]
+  test[,cluster_type:=paste0(cluster,type)]
+  
+  # subset training data for type that needs to be averaged over all stores in a particular cluster
+  cluster_type_na = unique(test[is.na(mkt_Type_store_predict),]$cluster_type)
+  temp = train[cluster_type %in% cluster_type_na]
+  train_type_average = unique(temp[,.(cluster_type,mkt_Type_store_predict=mean(mkt_Type_store),
+                                       mkt_Type_store_sales_predict=mean(mkt_Type_store_sales))
+                                    , by = .(cluster,type)])
+  #add storeIDs
+  store_cluster_type = unique(test[is.na(mkt_Type_store_predict),.(cluster_type,storeID)])
+  #merge the calculated mkt based on cluster and type
+  store_cluster_type = merge(store_cluster_type,
+                              train_type_average[,.(cluster_type,mkt_Type_store_sales_predict,mkt_Type_store_predict)], 
+                              by ="cluster_type",all.x = TRUE)
+  #merge the averaging estimation for every store if mkt share is NA
+  test[,mkt_Type_store_predict:=ifelse(is.na(mkt_Type_store_predict),
+                                        store_cluster_type[cluster_type==cluster_type,mkt_Type_store_predict],
+                                        mkt_Type_store_predict)]
+  
+  test[,mkt_Type_store_sales_predict:=ifelse(is.na(mkt_Type_store_sales_predict),
+                                              store_cluster_type[cluster_type==cluster_type,mkt_Type_store_sales_predict],
+                                              mkt_Type_store_sales_predict)]
+  
+  #remove auxiliary cluster
+  rm(train_type_average,store_cluster_type,temp)
+  
+  #Null the auxiliary columns 
+  train[,cluster_type:=NULL]
+  test[,cluster_type:=NULL]
   
   
   
@@ -206,15 +264,13 @@ mkt_MSE = function(train,test, cluster,avg_flavor,avg_groups){
 ##############################################################################################################################
 
 #the CV_represent function 
-clusters = readRDS("kmeans_clusters.RData")
-master = readRDS("master_mktshare.RData")
-
 
 CV_represent = function(master_table, number_of_CV,clusters_df){
 ## initialize empty list to keep the mse's
 mse_list = list()
 ## Loop through the different train and test data  
  for (i in 1:number_of_CV){
+   cat("CV",i)
    master <- master[,storeprodID:=paste0(storeID, productID)]
    
    #make sure that all test storeIDs already exist in train data
@@ -243,8 +299,10 @@ mse_list = list()
    #vector containing the row names of the above matrix  
    rnames = c()
    for(j in 2:length(clusters_df)){
+     cat("cluster",j)
      #isolate cluster
      cluster = clusters_df[,c(1,j)]
+     names(cluster)[2] = "cluster"
      #calculate mse using the above function
      new_mse = mkt_MSE(train,test, cluster,avg_flavor,avg_groups)
      #append to the mse matrix
@@ -258,9 +316,61 @@ mse_list = list()
    mse_list[[i]] = as.data.frame(mse)
    
  }
-  
-return(mse_list)
+
+### RESULTS 
+average_MSE = mse_list[[1]]
+for (l in 2:length(mse_list)){
+  average_MSE = average_MSE + mse_list[[l]]
 }
+
+average_MSE = average_MSE/number_of_CV
+
+average_average_MSE = rowSums(average_MSE)/ncol(average_MSE)
+cl = names(which.min(average_average_MSE))
+  
+return(list(cl,average_MSE))
+}
+
+######################################################################################################################################
+####################### Evaluate the clustering ######################################################################################
+
+#identify which grups has only 1 or 2 or 3 products 
+products = readRDS("products.RData")
+products = products[availability == "alltimer"]
+group_statistics  = products[,.(num_products = length(productID)), by = grup]
+avg_groups = c()
+for(i in group_statistics$grup){
+  if (group_statistics[grup==i,]$num_products == 1 |
+      group_statistics[grup==i,]$num_products == 2 |
+      group_statistics[grup==i,]$num_products == 3){
+    avg_groups = c(avg_groups,i)
+  }
+}
+
+
+#identify which flavor has only 1 or 2 or 3 products 
+flavor_statistics  = products[,.(num_products = length(productID)), by = flavor]
+avg_flavor= c()
+for(i in flavor_statistics$flavor){
+  if (flavor_statistics[flavor==i,]$num_products == 1 |
+      flavor_statistics[flavor==i,]$num_products == 2 |
+      flavor_statistics[flavor==i,]$num_products == 3){
+    avg_flavor = c(avg_flavor,i)
+  }
+}
+
+rm(products,flavor_statistics,group_statistics)
+
+
+
+## load the tables for evaluation
+clusters = readRDS("cluster.RData")
+master = readRDS("master_mktshare.RData")
+
+results = CV_represent(master_table = master,number_of_CV = 100,clusters_df = clusters)
+
+
+
 
 
 
